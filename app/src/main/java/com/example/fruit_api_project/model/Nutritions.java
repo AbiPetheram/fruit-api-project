fc2dd202437c0cee6dc94ec3d48aaebd1da8cafc
AbiPetheram <abi.gill97@gmail.com>
2024-09-17 11:20:44 +0100

    feat: add properties & constructor to Nutritions class

--- app/src/main/java/com/example/fruit_api_project/model/Nutritions.java
+++ app/src/main/java/com/example/fruit_api_project/model/Nutritions.java
@@ -1,4 +1,20 @@
 package com.example.fruit_api_project.model;
 
 public class Nutritions {
+    private int calories;
+    private double fat;
+    private double sugar;
+    private double carbohydrates;
+    private double protein;
+
+    public Nutritions(int calories, double fat, double sugar, double carbohydrates, double protein) {
+        this.calories = calories;
+        this.fat = fat;
+        this.sugar = sugar;
+        this.carbohydrates = carbohydrates;
+        this.protein = protein;
+    }
+
+    public Nutritions() {
+    }
 }
