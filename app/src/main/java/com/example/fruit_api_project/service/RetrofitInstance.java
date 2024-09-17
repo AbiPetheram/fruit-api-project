19afb7ae10496a8d6dc857f520e9db756f14821c
AbiPetheram <abi.gill97@gmail.com>
2024-09-17 11:47:26 +0100

    feat: add OkHttpClient implementation into getService

--- app/src/main/java/com/example/fruit_api_project/service/RetrofitInstance.java
+++ app/src/main/java/com/example/fruit_api_project/service/RetrofitInstance.java
@@ -1,5 +1,7 @@
 package com.example.fruit_api_project.service;
 
+import okhttp3.OkHttpClient;
+import okhttp3.logging.HttpLoggingInterceptor;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
@@ -8,10 +10,17 @@ public class RetrofitInstance {
     private static final String BASE_URL = "http://fruityvice.com/api/fruit/all/";
 
     public static ApiService getService(){
+        HttpLoggingInterceptor logging = new HttpLoggingInterceptor();
+        logging.setLevel(HttpLoggingInterceptor.Level.BODY);
+        OkHttpClient client = new OkHttpClient.Builder()
+                .addInterceptor(logging)
+                .build();
+
         if(retrofitInstance == null){
             Retrofit retrofit = new Retrofit.Builder()
                     .baseUrl(BASE_URL)
                     .addConverterFactory(GsonConverterFactory.create())
+                    .client(client)
                     .build();
 
         }
