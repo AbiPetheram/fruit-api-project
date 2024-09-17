7f5bdc26d0efe7ea9b3be4b4c9d531027283d54a
AbiPetheram <abi.gill97@gmail.com>
2024-09-17 11:21:46 +0100

    feat: add getter and setters for properties in Nutritions

--- app/src/main/java/com/example/fruit_api_project/model/Nutritions.java
+++ app/src/main/java/com/example/fruit_api_project/model/Nutritions.java
@@ -17,4 +17,44 @@ public class Nutritions {
 
     public Nutritions() {
     }
+
+    public int getCalories() {
+        return calories;
+    }
+
+    public void setCalories(int calories) {
+        this.calories = calories;
+    }
+
+    public double getFat() {
+        return fat;
+    }
+
+    public void setFat(double fat) {
+        this.fat = fat;
+    }
+
+    public double getSugar() {
+        return sugar;
+    }
+
+    public void setSugar(double sugar) {
+        this.sugar = sugar;
+    }
+
+    public double getCarbohydrates() {
+        return carbohydrates;
+    }
+
+    public void setCarbohydrates(double carbohydrates) {
+        this.carbohydrates = carbohydrates;
+    }
+
+    public double getProtein() {
+        return protein;
+    }
+
+    public void setProtein(double protein) {
+        this.protein = protein;
+    }
 }
