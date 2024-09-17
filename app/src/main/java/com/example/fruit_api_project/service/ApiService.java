811837e9a1dd2e8ce71f83760feb39e242a985c5
AbiPetheram <abi.gill97@gmail.com>
2024-09-17 11:51:22 +0100

    feat: add getAllFruits method to ApiService interface

--- app/src/main/java/com/example/fruit_api_project/service/ApiService.java
+++ app/src/main/java/com/example/fruit_api_project/service/ApiService.java
@@ -1,2 +1,13 @@
-package com.example.fruit_api_project.service;public interface ApiService {
+package com.example.fruit_api_project.service;
+
+import com.example.fruit_api_project.model.Fruit;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+
+public interface ApiService {
+    @GET
+    Call<List<Fruit>> getAllFruit();
 }
