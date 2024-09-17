37c1664001ee8bed2674256f1a6a70f186c0bfbe
AbiPetheram <abi.gill97@gmail.com>
2024-09-17 11:43:31 +0100

    feat: add getService method with retrofit builder implementation

--- app/src/main/java/com/example/fruit_api_project/service/RetrofitInstance.java
+++ app/src/main/java/com/example/fruit_api_project/service/RetrofitInstance.java
@@ -1,2 +1,19 @@
-package com.example.fruit_api_project.service;public class RetrofitInstance {
+package com.example.fruit_api_project.service;
+
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+public class RetrofitInstance {
+    private static RetrofitInstance retrofitInstance = null;
+    private static final String BASE_URL = "http://fruityvice.com/api/fruit/all/";
+
+    public static ApiService getService(){
+        if(retrofitInstance == null){
+            Retrofit retrofit = new Retrofit.Builder()
+                    .baseUrl(BASE_URL)
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .build();
+
+        }
+    }
 }
